cd016f30805d0ddea4a9e52c73ad66447628eee0
Dalgafirik <muhammedeneskarakaya@gmail.com>
2026-01-05 18:59:33 +0000

    Initial Docker Compose setup with env configuration

--- service/app.py
+++ service/app.py
@@ -1,10 +1,21 @@
+import os
 from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+APP_NAME = os.getenv("APP_NAME", "devops-lab")
+APP_ENV = os.getenv("APP_ENV", "local")
+APP_PORT = int(os.getenv("APP_PORT", 5000))
+
 @app.route("/")
 def home():
-    return jsonify({"message": "Hello DevOps Lab"})
+    return jsonify({
+        "app": APP_NAME,
+        "environment": APP_ENV,
+        "port": APP_PORT
+    })
+
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=APP_PORT)
+
